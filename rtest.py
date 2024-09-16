92e9ce13e1b3215e67f706018ef003105a4002d9
Vasil-Kirov <vaskoabs@gmail.com>
2024-09-16 22:24:46 +0200

    added new test for dynamic arrays

--- rtest.py
+++ rtest.py
@@ -23,7 +23,7 @@ def remove_newlines(string):
     return string
 
 def get_tests():
-    paths = ['arrays', 'basic', 'fn_call', 'fn_ptr', 'pointers', 'struct', 'pass_struct', 'struct_in_struct', 'lambda', 'pass_complex', 'iterators', 'slices', 'var_args', 'loop_and_if', 'generics', 'defer', 'if_else']
+    paths = ['arrays', 'basic', 'fn_call', 'fn_ptr', 'pointers', 'struct', 'pass_struct', 'struct_in_struct', 'lambda', 'pass_complex', 'iterators', 'slices', 'var_args', 'loop_and_if', 'generics', 'defer', 'if_else', 'dynamic_array']
     paths.sort()
     return paths
 

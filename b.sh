81b92945448933e01dc75cab060b3a715d9c5033
Vasil Kirov <vaskoabs@gmail.com>
2025-02-02 19:56:07 +0100

    flags for release build in the build script

--- b.sh
+++ b.sh
@@ -4,13 +4,23 @@ DEFINES="-D_CRT_SECURE_NO_WARNINGS -DDEBUG -DCM_LINUX"
 LIBS=$(llvm-config --ldflags --libs)
 LIBS+=' -ldyncall_s'
 
-DO_ASAN='-fsanitize=address,undefined'
+
+if [[ "$1" == "rel" ]]
+then
+	FLAGS='-O3'
+else
+
+	FLAGS='-fsanitize=address,undefined --debug -O0'
+fi
+
 #DO_ASAN=''
 
 #export ASAN_OPTIONS=detect_leaks=0
 
+echo $FLAGS
+
 pushd bin
-clang++ $DO_ASAN -rdynamic -orcp --debug -lz ../src/Main.cpp -I../include -I../src $LIBS $DEFINES -mavx -Wall -Wno-format
+clang++ $DO_ASAN -rdynamic -orcp $FLAGS -lz ../src/Main.cpp -I../include -I../src $LIBS $DEFINES -mavx -Wall -Wno-format
 popd
 
 

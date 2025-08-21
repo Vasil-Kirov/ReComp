9791a9b9a97019a6701cf315c1cf1b9647731329
Vasil-Kirov <vaskoabs@gmail.com>
2025-08-21 18:34:25 +0200

    cleaning up project

--- data/b.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-
-:: --vmdll ..\bin\testdll.dll 
-
-..\bin\rcp.exe build.rcp %1 %2 %3 --link kernel32.lib
-REM LINK pass.obj /NOLOGO /DEFAULTLIB:LIBCMT /ENTRY:mainCRTStartup
-REM LINK "!internal.obj" main.obj file2.obj /NOLOGO /DEFAULTLIB:LIBCMT /ENTRY:mainCRTStartup /OUT:a.exe
-
--- data/b.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rcp build.rcp $1 $2 $3 --link -lm || exit 1
-
